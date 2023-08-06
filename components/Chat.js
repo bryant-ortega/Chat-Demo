68a36feaabb477190b4723f000836f074117370a
Bryant Ortega <kalicorose@gmail.com>
2023-08-06 03:22:08 -0700

    background change and style updates

--- components/Chat.js
+++ components/Chat.js
@@ -3,15 +3,24 @@ import { StyleSheet, View, Text } from "react-native";
 
 const Chat = ({ route, navigation }) => {
 
-  const { name } = route.params;
+  const { name, color } = route.params;
 
   useEffect(() => {
     navigation.setOptions({ title: name });
   }, []);
 
     return (
-        <View style={styles.container}>
-            <Text>Chat!</Text>
+        <View style={[{ backgroundColor: color }, styles.container]}>
+            {/*makes sure text is legible w various colors of backgrounds */}
+            <Text
+                style={
+                    color !== "white"
+                        ? [{ color: "white" }, styles.title]
+                        : styles.title
+                }
+            >
+                Let's Chat!
+            </Text>
         </View>
     );
 };
@@ -22,6 +31,10 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
+    title: {
+      fontWeight: 'bold',
+      fontSize: 30,
+    }
 });
 
 export default Chat;
--- components/Start.js
+++ components/Start.js
@@ -1,64 +1,142 @@
-import { StyleSheet, View, Text, Button, TextInput, TouchableOpacity } from "react-native";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    ImageBackground,
+} from "react-native";
 import { useState } from "react";
 
+const image = require("../assets/Background_Image.png");
 
-
+//states passed from Start screen to Chat screen
 const Start = ({ navigation }) => {
-
-  const [name, setName] = useState('');
+    const [name, setName] = useState("");
+    const [color, setColor] = useState("");
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.plainText}>Start Screen!</Text>
-            <TextInput
-                style={styles.textInput}
-                value={name}
-                onChangeText={setName}
-                placeholder="Enter your first name"
-            />
-            <TouchableOpacity
-                style={styles.button}
-                title="Go to Chat"
-                onPress={() => navigation.navigate("Chat", { name: name })}
-            >
-                <Text style={styles.buttonText}>Go to Chat</Text>
-            </TouchableOpacity>
-        </View>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+            <View style={styles.container}>
+                <View style={styles.subContainer}>
+                    <Text style={[styles.plainText, styles.setColorWhite]}>
+                        Chat Demo
+                    </Text>
+                </View>
+                <View style={styles.subContainer}>
+                    <TextInput
+                        style={styles.textInput}
+                        value={name}
+                        onChangeText={setName}
+                        placeholder="Your name"
+                    />
+                    <Text>Choose Background Color</Text>
+                    {/*this container is required to keep radio buttons in line */}
+                    <View style={styles.radioButtonContainer}>
+                        <TouchableOpacity
+                            style={[
+                                styles.radioButton,
+                                { backgroundColor: "#090C08" },
+                            ]}
+                            onPress={() => setColor("#090C08")}
+                        ></TouchableOpacity>
+                        <TouchableOpacity
+                            style={[
+                                styles.radioButton,
+                                { backgroundColor: "#474056" },
+                            ]}
+                            onPress={() => setColor("#474056")}
+                        ></TouchableOpacity>
+                        <TouchableOpacity
+                            style={[
+                                styles.radioButton,
+                                { backgroundColor: "#8A95A5" },
+                            ]}
+                            onPress={() => setColor("#8A95A5")}
+                        ></TouchableOpacity>
+                        <TouchableOpacity
+                            style={[
+                                styles.radioButton,
+                                { backgroundColor: "#B9C6AE" },
+                            ]}
+                            onPress={() => setColor("#B9C6AE")}
+                        ></TouchableOpacity>
+                    </View>
+                    {/*passes states to Chat and adds defaults for when nothing is entered or selected*/}
+                    <TouchableOpacity
+                        style={styles.button}
+                        title="Go to Chat"
+                        onPress={() =>
+                            navigation.navigate("Chat", {
+                                name: name ? name : "User",
+                                color: color ? color : "white",
+                            })
+                        }
+                    >
+                        <Text style={styles.buttonText}>Start chatting</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+    },
     container: {
         flex: 1,
         alignItems: "center",
-        marginTop: 15,
-        marginBottom: 10,
+        justifyContent: "center",
+    },
+    subContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        width: "88%",
     },
     plainText: {
         height: 80,
         fontWeight: "600",
-        fontSize: 44,
+        fontSize: 45,
         padding: 10,
     },
     textInput: {
-        width: "76%",
+        width: "88%",
         padding: 15,
-        borderWidth: 1,
+        fontWeight: "300",
+        fontSize: 16,
+        borderWidth: 3,
         marginTop: 15,
-        marginBottom: 35,
+        marginBottom: 30,
+    },
+    radioButtonContainer: {
+        width: "70%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        margin: 30,
+    },
+    radioButton: {
+        backgroundColor: "#FFFFFF",
+        width: 30,
+        height: 30,
+        borderRadius: 15,
     },
     button: {
         alignItems: "center",
-        width: "50%",
+        width: "99%",
         paddingVertical: 20,
-        borderWidth: 1,
-        backgroundColor: "lightblue",
-        borderRadius: 50,
+        backgroundColor: "#757083",
+        borderRadius: 10,
     },
     buttonText: {
-        fontSize: 20,
+        fontSize: 16,
         fontWeight: "600",
-        color: "#000",
+        color: "#FFFFFF",
+    },
+    setColorWhite: {
+        color: "#FFFFFF",
     },
 });
 
