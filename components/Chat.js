2778433ea566f9a3b8aaf5bbfbd807d78ee37e85
Bryant Ortega <kalicorose@gmail.com>
2023-08-07 19:55:58 -0700

    added Gifted Chat to chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,48 @@
-import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
+    const [messages, setMessages] = useState([]);
+    const { name, color } = route.params;
 
-  const { name, color } = route.params;
+    // called when a user sends a message
+    const onSend = newMessages => {
+        setMessages(previousMessages =>
+            GiftedChat.append(previousMessages, newMessages)
+        );
+    };
 
-  useEffect(() => {
-    navigation.setOptions({ title: name });
-  }, []);
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                },
+            },
+        ]);
+        //displays name at the top of chat screen
+        navigation.setOptions({ title: name });
+    }, []);
 
     return (
         <View style={[{ backgroundColor: color }, styles.container]}>
             {/*makes sure text is legible w various colors of backgrounds */}
-            <Text
-                style={
-                    color !== "white"
-                        ? [{ color: "white" }, styles.title]
-                        : styles.title
-                }
-            >
-                Let's Chat!
-            </Text>
+            <GiftedChat
+                messages={messages}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+            {Platform.OS === "android" ? (
+                <KeyboardAvoidingView behavior="height" />
+            ) : null}
         </View>
     );
 };
@@ -32,9 +54,9 @@ const styles = StyleSheet.create({
         alignItems: "center",
     },
     title: {
-      fontWeight: 'bold',
-      fontSize: 30,
-    }
+        fontWeight: "bold",
+        fontSize: 30,
+    },
 });
 
 export default Chat;
