e6ad300fb6ff037bdad916c6ccff73ca2441858a
Bryant Ortega <kalicorose@gmail.com>
2023-08-11 17:26:56 -0700

    fixed firebaseConfig again

--- App.js
+++ App.js
@@ -13,15 +13,17 @@ import { getFirestore } from "firebase/firestore";
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDMqYQ5nTthzBd4E669Ku4Oe3l-7uQaVys",
-  authDomain: "chatapp-11e31.firebaseapp.com",
-  projectId: "chatapp-11e31",
-  storageBucket: "chatapp-11e31.appspot.com",
-  messagingSenderId: "317111370639",
-  appId: "1:317111370639:web:07c5d289e3f3fe7a3c81fa",
-  measurementId: "G-154MGBMRTP"
-};
+const App = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDMqYQ5nTthzBd4E669Ku4Oe3l-7uQaVys",
+        authDomain: "chatapp-11e31.firebaseapp.com",
+        projectId: "chatapp-11e31",
+        storageBucket: "chatapp-11e31.appspot.com",
+        messagingSenderId: "317111370639",
+        appId: "1:317111370639:web:07c5d289e3f3fe7a3c81fa",
+        measurementId: "G-154MGBMRTP",
+    };
+
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
 
